ffd6bf2df2269e65d534299a346782d267dd26de
Moon Yun Ha <dbsgk47@sookmyung.ac.kr>
2023-06-20 23:18:35 +0900

    백엔드 개발 18. @NamedQuery 사용하기

--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -10,11 +10,11 @@ public class TodoService {
 		public String testService(){
 				// Todo Entity 생성
 				TodoEntity entity = TodoEntity.builder().userId("user01").title("My first todo item").build();
-				// Todo Entity 저장
+				//Todo Entity 저장
 				repository.save(entity);
-				// Todo Entity 검색
+				//Todo Entity 검색
 				TodoEntity savedEntity = 
-repository.findByUserId(entity.getUserId()).get(0);
+repository.searchByUserId(entity.getUserId()).get(0);
 				return savedEntity.getUserId();
 		}
 }
